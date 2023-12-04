656426ef00db2c5214e5e0162f0f0c328042464d
kenny <myemail@com>
2023-12-04 19:06:26 +0100

    Remove unused union

--- server.c
+++ server.c
@@ -24,15 +24,6 @@ struct Twitter {
     struct Record posts[];
 }* twitter;
 
-/* this structure was copied from semctl(2) manual */
-union semun {
-    int val;               /* Value for SETVAL */
-    struct semid_ds* buf;  /* Buffer for IPC_STAT, IPC_SET */
-    unsigned short* array; /* Array for GETALL, SETALL */
-    struct seminfo* __buf; /* Buffer for IPC_INFO
-                              (Linux-specific) */
-};
-
 void cleanup(int sig);
 
 void sys_err(const char* errmsg) {
