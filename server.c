d838facd1714d8ba86d264d037329b9742679a95
kenny <myemail@com>
2023-12-04 15:35:59 +0100

    Use POSIX named semaphores for server

--- server.c
+++ server.c
@@ -1,10 +1,11 @@
 #include <errno.h>
+#include <fcntl.h>
+#include <linux/limits.h>
+#include <semaphore.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
 #include <sys/shm.h>
 #include <sys/types.h>
 
@@ -45,20 +46,16 @@ void sys_err_with_cleanup(const char* errmsg) {
 }
 
 int shmid;
-int semid;
+sem_t** semset;
+const char* keyfile;
 
 void print_posts(int sig) {
-    struct sembuf alloc_size = {twitter->capacity, -1, 0};
-    struct sembuf free_size = {twitter->capacity, 1, 0};
-    struct sembuf alloc_record = {0, -1, 0};
-    struct sembuf free_record = {0, 1, 0};
     int i;
 
     printf("\n");
 
-    if (semop(semid, &alloc_size, 1) == -1) {
-        perror("Nie mozna zajac zasobu");
-        return;
+    if (sem_wait(semset[twitter->capacity]) == -1) {
+        sys_err("Nie mozna zarezerwowac rozmiaru");
     }
 
     if (twitter->size == 0) {
@@ -66,72 +63,125 @@ void print_posts(int sig) {
     } else {
         printf("_____________  Twitter 2.0:  _____________\n");
         for (i = 0; i < twitter->size; ++i) {
-            alloc_record.sem_num = free_record.sem_num = i;
-
-            if (semop(semid, &alloc_record, 1) == -1) {
-                perror("Nie mozna zwolnic zasobu");
-                return;
+            if (sem_wait(semset[i]) == -1) {
+                sys_err("Nie mozna zarezerwowac postu");
             }
 
             printf("%d. [%s]: %s [Polubienia: %d]\n", i + 1,
                    twitter->posts[i].username, twitter->posts[i].post,
                    twitter->posts[i].likes);
 
-            if (semop(semid, &free_record, 1) == -1) {
-                perror("Nie mozna zwolnic zasobu");
-                return;
+            if (sem_post(semset[i]) == -1) {
+                sys_err("Nie mozna zwolnic rezerwacji postu");
             }
         }
     }
 
-    if (semop(semid, &free_size, 1) == -1) {
-        perror("Nie mozna zwolnic zasobu");
-        return;
+    if (sem_post(semset[twitter->capacity]) == -1) {
+        sys_err("Nie mozna zwolnic rezerwacji rozmiaru");
     }
 }
 
-void cleanup(int sig) {
-    printf("[SERWER]: dostalem SIGINT => koncze i sprzatam... ");
-    printf("odlaczanie: %s, usuniecie: %s\n",
-           (shmdt(twitter) == 0) ? "OK" : strerror(errno),
-           (shmctl(shmid, IPC_RMID, 0) == 0) ? "OK" : strerror(errno));
-    printf("usuwanie semafor: %s\n",
-           (semctl(semid, IPC_RMID, 0) == 0) ? "OK" : strerror(errno));
-    exit(EXIT_SUCCESS);
-}
-
-int init_semset(int nsem, char* keyfile, int id) {
-    key_t semkey;
-    int semsetid;
+int semset_close(sem_t** semset, int nsem) {
+    // TODO: on error try to close rest of semaphores
+    // +1 because on this pos we hold size
     int i;
-
-    union semun semval;
-    semval.val = 1;
-
-    if ((semkey = ftok(keyfile, id)) == -1) {
-        sys_err_with_cleanup("Nie udalo sie wygenerowac klucza dla semafory");
-    }
-
-    /* jeden wiecej dla kontrolowania rozmiaru i pojemnosci */
-    if ((semsetid = semget(semkey, nsem + 1, 0666 | IPC_CREAT)) == -1) {
-        sys_err_with_cleanup("Nie mozna utworzyc zbioru semafor");
-    }
-
-    for (i = 0; i < nsem + 1; ++i) { /* + 1 zeby zainicjalizowac tez rozmiar */
-        if (semctl(semsetid, i, SETVAL, semval) == -1) {
-            sys_err_with_cleanup("Nie mozna zainicjalizowac semafory");
+    for (i = 0; i < nsem + 1; ++i) {
+        if (sem_close(semset[i]) == -1) {
+            return -1;
         }
     }
 
-    return semsetid;
+    return 0;
 }
 
-int create_shm(int nposts, char* keyfile, int id) {
+int semset_unlink(const char* key, int nsem) {
+    int i;
+    int is_success;
+
+    char name[NAME_MAX] = "/";
+    strcat(name, key);
+
+    char* semnum = name + strlen(name);
+
+    is_success = 0;
+    for (i = 0; i < nsem; ++i) {
+        sprintf(semnum, "%d", i);
+
+        if (sem_unlink(name) == -1) {
+            is_success = -1;
+            perror(name);
+            /*remove rest of files thus not fatal */
+        }
+    }
+
+    // INFO: special semaphore to reserve size
+    strcpy(semnum, "size");
+    if (sem_unlink(name) == -1) {
+        is_success = -1;
+        perror(name);
+    }
+
+    return is_success;
+}
+
+void cleanup(int sig) {
+    printf("[SERWER]: dostalem SIGINT => koncze i sprzatam...\n");
+
+    printf("semafory: odlaczanie: %s, ",
+           (semset_close(semset, twitter->capacity) == 0) ? "OK"
+                                                          : strerror(errno));
+    free(semset);
+    printf("Zwolniono pamiec, ");
+    printf("usuwanie semafor: %s\n",
+           (semset_unlink(keyfile, twitter->capacity) == 0) ? "OK"
+                                                            : strerror(errno));
+
+    printf("shared memory: odlaczanie: %s, usuniecie: %s\n",
+           (shmdt(twitter) == 0) ? "OK" : strerror(errno),
+           (shmctl(shmid, IPC_RMID, 0) == 0) ? "OK" : strerror(errno));
+    // TODO: cleanup
+
+    exit(EXIT_SUCCESS);
+}
+
+// WARNING: returned semaphore set should be freed
+sem_t** create_semset(int nsem, const char* key, int id) {
+    sem_t** semset;
+    int i;
+
+    if ((semset = calloc(nsem + 1, sizeof(sem_t*))) == NULL) {
+        sys_err("Can't alloc memory for semaphore set");
+    }
+
+    char name[NAME_MAX] = "/";
+    strcat(name, key);
+    char* semnum = name + strlen(name);
+
+    for (i = 0; i < nsem; ++i) {
+        sprintf(semnum, "%d", i);
+        semset[i] = sem_open(name, O_CREAT, 0666, 1);
+        if (semset[i] == SEM_FAILED) {
+            sys_err(name);
+        }
+    }
+
+    // INFO: special semaphore to reserve size
+    strcpy(semnum, "size");
+    semset[nsem] = sem_open(name, O_CREAT, 0666, 1);
+    if (semset[nsem] == SEM_FAILED) {
+        sys_err(name);
+    }
+
+    return semset;
+}
+
+int create_shm(int nposts, const char* key, int id) {
     key_t shmkey;
     int sid;
 
-    printf("[SERWER]: tworze klucz na podstawie pliku %s... ", keyfile);
-    shmkey = ftok(keyfile, id);
+    printf("[SERWER]: tworze klucz na podstawie pliku %s... ", key);
+    shmkey = ftok(key, id);
     if (shmkey == -1) {
         sys_err("Nie udalo sie wygenerowac klucza dla pamieci dzielonej");
     }
@@ -189,12 +239,13 @@ int main(int argc, char* argv[]) {
     if (errno != 0) {
         sys_err("Drugim argumentem powinnna byc liczba calkowita");
     }
+    keyfile = argv[1];
 
     printf("[SERWER]: Twitter 2.0 (Wersja A)\n");
-    shmid = create_shm(nposts, argv[1], 1);
+    shmid = create_shm(nposts, keyfile, 1);
     print_shminfo(shmid);
 
-    semid = init_semset(nposts, argv[1], 2);
+    semset = create_semset(nposts, keyfile, 2);
 
     twitter = init_twitter(shmid, nposts);
 
